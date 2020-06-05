e32798ff79f3891f880f877e0a51394b2dfac764
Astrid Varga <astrid.varga@idealo.de>
2020-06-05 10:27:05 +0200

    setup prettier and reformat files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter'
+
+import App from './App'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+    const { getByText } = render(<App />)
+    const linkElement = getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+})
