34566e7b80d59ee98b38ca1ccae04f8b8b72437a
Astrid Varga <astrid.varga@idealo.de>
2020-06-18 16:58:32 +0200

    refactor test to function

--- src/App.test.js
+++ src/App.test.js
@@ -13,33 +13,42 @@ Enzyme.configure({ adapter: new EnzymeAdapter() })
  * @param {any} state - Initial state for setup
  * @returns {ShallowWrapper}
  */
-
 const setup = (props = {}, state = null) => {
     return shallow(<App {...props} />)
 }
 
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {*} value - value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttribute = (wrapper, value) => {
+    return wrapper.find(`[data-test="${value}"]`)
+}
+
 test('renders without crashing', () => {
     const wrapper = setup()
-    const appComponent = wrapper.find("[data-test='component-app']")
+    const appComponent = findByTestAttribute(wrapper, 'component-app')
     expect(appComponent.length).toBe(1)
 })
 test('renders increment button', () => {
     const wrapper = setup()
-    const button = wrapper.find("[data-test='increment-button']")
+    const button = findByTestAttribute(wrapper, 'increment-button')
     expect(button.length).toBe(1)
 })
 test('renders counter display', () => {
     const wrapper = setup()
-    const counterDisplay = wrapper.find("[data-test='counter-display']")
+    const counterDisplay = findByTestAttribute(wrapper, 'counter-display')
     expect(counterDisplay.length).toBe(1)
 })
 test('counter starts at 0', () => {
     const wrapper = setup()
-    const appComponent = wrapper.find("[data-test='component-app']")
+    const appComponent = findByTestAttribute(wrapper, 'component-app')
     expect(appComponent.length).toBe(1)
 })
 test('clicking button increments the counter', () => {
     const wrapper = setup()
-    const appComponent = wrapper.find("[data-test='component-app']")
+    const appComponent = findByTestAttribute(wrapper, 'component-app')
     expect(appComponent.length).toBe(1)
 })
